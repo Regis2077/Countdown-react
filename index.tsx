c1f5849cc1dce09722473217bc4362c16931ec8b
Gabriel Regis <86062383+Regis2077@users.noreply.github.com>
2022-09-24 16:48:00 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,33 @@
+interface CountdownReactProps{
+  initialSeconds?: number
+}
+
+const CountdownReact  = ({initialSeconds = 1800}:CountdownReactProps) =>{
+ const [seconds, setSeconds] = useState(initialSeconds)
+
+   const countDownText = useMemo(() => {
+     const oneMinuteInSeconds = 60;
+     const remainingMinutes = Math.floor(seconds / oneMinuteInSeconds);
+     const remainingSeconds = seconds - remainingMinutes * 60;
+     const remainingMinutesStr = remainingMinutes.toString().padStart(2, '0')
+     const remainingSecondsStr = remainingSeconds.toString().padStart(2, '0')
+
+     return `${remainingMinutesStr}:${remainingSecondsStr}`;
+   }, [seconds])
+
  useEffect(()=>{
-    const oneSecondInMs = 1000;
+     const oneSecondInMs = 1000;
 
-    const timeoutId = setInterval(() => {
-      if (seconds - 1 < 0) {
-        clearInterval(timeoutId);
-        return;
-      }
-      setSeconds(seconds - 1)
-    }, oneSecondInMs)
+     const timeoutId = setInterval(() => {
+       if (seconds - 1 < 0) {
+         clearInterval(timeoutId);
+         return;
+       }
+       setSeconds(seconds - 1)
+     }, oneSecondInMs)
 
-    return function cleanup() {
-      clearInterval(timeoutId)
-    };
-  },[seconds])
+     return function cleanup() {
+       clearInterval(timeoutId)
+     };
+   },[seconds])
+}
